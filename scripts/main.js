6d2a6b1c54a88f6674f19eb06c627137707dc8f1
Evan Emory <evanemory2007@yahoo.com>
2024-03-28 11:36:50 -0500

    Updated Main, Transient State, Database

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+
+const container = document.querySelector("#container")
+
+
+
+
+const render = async () => {
+
+    // Call the functions created inside of other module
+
+
+
+
+    container.innerHTML = `
+
+                          `
+}
+
+
+document.addEventListener("stateChanged", render)
+
+render()
